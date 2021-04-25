39dc2358ea5c3fbbdbc4f038bfc2ea3662021ff5
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-04-25 08:51:04 +0200

    return list

---
+++ ProductivityTools.ImageGallery.Api/Controllers/ImageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductivityTools.ImageGallery.Api.Model;
+
+namespace ProductivityTools.ImageGallery.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private string BasePath = @"d:\.PawelPC\Photographs\Processed\zdjeciaDone\2009.12.30 Sylwester\";
+
+        [HttpGet]
+        [Route("List")]
+        public List<ImageItem> List()
+        {
+            var result = new List<ImageItem>();
+            string[] files = Directory.GetFiles(BasePath);
+            foreach (string file in files)
+            {
+                result.Add(new ImageItem { Original = file, Thumbnail = file });
+            }
+            return result;
+        }
+
+        [HttpGet]
+        [Route("Image")]
+        public IActionResult Get(string name)
+        {
+            string path = Path.Join(BasePath, name);
+            PhysicalFileResult result = PhysicalFile(path, "image/jpg");
+            return result;
+        }
+    }
+}
---
+++ ProductivityTools.ImageGallery.Api/Model/ImageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductivityTools.ImageGallery.Api.Model
+{
+    public class ImageItem
+    {
+        public string Original { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
