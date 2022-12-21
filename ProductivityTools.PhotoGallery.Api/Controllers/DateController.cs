d368cb1d5240b07897b5cbf0ad905a066b3cbf6b
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-12-21 12:47:32 +0100

    date controller

---
+++ ProductivityTools.PhotoGallery.Api/Controllers/DateController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ProductivityTools.PhotoGallery.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateController : ControllerBase
+    {
+        //https://localhost:5001/api/Images/List
+        [HttpGet]
+        [Route("Get")]
+        public string Get()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
